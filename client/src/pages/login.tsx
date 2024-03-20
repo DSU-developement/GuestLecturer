5d0a2fca6858eeb8a9025822b0aecab2fd60e25d
Nuthan B <NuthanB@protonmail.com>
2024-03-20 20:56:01 +0530

    Signup button fixed

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -105,7 +105,7 @@ const LoginPage = () => {
             {errorMessage && (
               <div className="text-red-500 text-center">{errorMessage}</div>
             )}
-            <div>
+            <div className='flex'>
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -115,7 +115,7 @@ const LoginPage = () => {
               <button
               type='button'
               onClick={handleSignup}
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group  ml-2 relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 SignUp
               </button>
