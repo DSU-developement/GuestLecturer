f736c96d2d0ec73051a00fe57fb8e06f9bb82904
Nuthan B <NuthanB@protonmail.com>
2024-03-26 18:23:40 +0530

    Added the college logo

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Collegelogo from "../assets/collegelogo.png";
 
 const Sidebar: React.FC = () => {
   return (
     <div className="bg-gray-800 text-white w-62 h-screen flex flex-col">
       <div className="p-4">
-        <div className="flex items-center justify-center h-16 text-2xl font-bold">
+        <img src={Collegelogo} className="w-28 mt-2 ml-5" alt='collegelogo'/>
+        <div className="flex items-center justify-center h-16 text-2xl font-bold ">
           DSU
         </div>
         <ul className="space-y-2">
@@ -14,7 +16,7 @@ const Sidebar: React.FC = () => {
               to="/add-guest-lecture"
               className="block py-2 px-4 rounded hover:bg-gray-700 transition duration-300"
             >
-              Add Guest Lecture
+              Accept Guest Lecture
             </Link>
           </li>
           <li>
