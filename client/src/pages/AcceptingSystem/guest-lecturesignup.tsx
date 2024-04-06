6e8201e163218afab80c638bd47cd0247b01cc4f
AnkithaKoushik <121386672+AnkithaKoushik@users.noreply.github.com>
2024-04-07 00:43:38 +0530

    guestlecturersignup page

--- client/src/pages/AcceptingSystem/guest-lecturesignup.tsx
+++ client/src/pages/AcceptingSystem/guest-lecturesignup.tsx
@@ -112,32 +112,39 @@ const SignupPageLect = () => {
   }, [submitted]);
   return (
     <div className='h-full flex flex-col bg-gray-100'>
-      <div className="md:max-w-4xl w-full container mx-auto p-6 rounded-xl m-6 bg-white" style={{ backgroundImage: `url("./bg.jpeg")` }}>
-        <div className='flex justify-center mb-4'>
-          <CgProfile className='text-6xl text-blue-600' />
-        </div>
-        <h2 className="text-4xl mb-4 text-center font-bold">Create a new Guest Lecturer account </h2>
-        <form onSubmit={handleSignup} className="space-y-4 p-3">
-          <div className='grid grid-cols-2 gap-6'>
-            <div className='bg-gray-100 rounded border-l-8  p-3 flex flex-col justify-items-start flex-wrap items-center'>
-              <h2 className='font-mono font-black text-2xl w-full font-bold text-blue-500 col-span-2 text-center mb-3'>PERSONAL DETAILS</h2>
-              <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Name" type="text" name="facultyName" value={formData.facultyName} onChange={handleChange} required />
+      <div className=" w-full container mx-auto p-6 rounded-xl m-6 bg-white" style={{ backgroundImage: `url("./bg.jpeg")` }} >
+        <h2 className="text-4xl mb-4 text-center font-bold">Create a New Guest Lecturer Account </h2>
+        <form onSubmit={handleSignup} className="space-y-4 p-3 ">
+          <div className='container mx-auto p-1 rounded-xl bg-gray-10'>
+          <div className='mt-3 border border-black bg-gray-50 rounded'>
+          <h2 className=' font-mono font-black text-2xl w-full font-bold text-blue-500  ml-3 mt-3'>PERSONAL DETAILS</h2>
+            <div className='grid grid-cols-3 gap-3 rounded  p-2 flex flex-col  flex-wrap '>
+              <div className='m-1 p-1'>
+                <input autoComplete='no' className='w-80 border border-black rounded p-2 outline-blue-600' placeholder="Name" type="text" name="facultyName" value={formData.facultyName} onChange={handleChange} required />
               </div>
-              <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Phone" type="text" name='phone' value={formData.phone} onChange={handleChange} required />
+              <div className='m-2 p-1'>
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 text-blue-700' placeholder="Phone" type="text" name='phone' value={formData.phone} onChange={handleChange} required />
               </div>
-              <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Email"  type="text" name='email' value={formData.email} onChange={handleChange} required />
+              <div className='m-2 p-1'>
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 text-blue-700' placeholder="Email"  type="text" name='email' value={formData.email} onChange={handleChange} required />
               </div>
-              <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="password" type="password" name="password" value={formData.password} onChange={handleChange} required />
+              <div className='m-2 p-1'>
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 text-blue-700' placeholder="Password" type="password" name="password" value={formData.password} onChange={handleChange} required />
               </div>
+            </div> 
             </div>
-            <div className='bg-gray-100 rounded border-l-8  p-3 flex flex-col justify-items-start flex-wrap items-center'>
-              <h2 className='font-mono font-black text-2xl w-full border-sky-200 font-bold text-blue-500 col-span-2 text-center mb-3'>ACADEMIC DETAILS</h2>
-              <div className='p-2 flex flex-col'>
-                <div className='flex'>
+            <div className='mt-3 border border-black bg-gray-50'>
+            <h2 className='font-mono font-black text-2xl w-full font-bold text-blue-500  ml-3'>ACADEMIC DETAILS</h2>
+            <div className='grid grid-cols-3'>              
+              <div className='grid grid-cols-3 gap-3 rounded  p-2 flex flex-col  flex-wrap'>
+              <div className='m-2 p-2 '>
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 ' placeholder="Schools/Deanery" type="text" name="schoolsDeanery" value={formData.schoolsDeanery} onChange={handleChange} />
+              </div>
+              </div>
+              <div className='m-2 p-2'>
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 ' placeholder="Department" type="text" name="department" value={formData.department} onChange={handleChange} required />
+              </div>
+              <div className='flex'>
                   <div className='m-3'>
                     <label className='font-mono font-bold text-xl text-blue-600 m-1'> UG</label>
                     <input className='w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600' type="checkbox" name="qualifications.ug" checked={formData.qualifications.ug} onChange={handleChange} />
@@ -151,44 +158,39 @@ const SignupPageLect = () => {
                     <input className='w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600' type="checkbox" name="qualifications.phd" checked={formData.qualifications.phd} onChange={handleChange} />
                   </div>
                 </div>
+              <div className='m-2 p-2'>
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 ' placeholder="Subject Name" type="text" name="subjectName" value={formData.subjectName} onChange={handleChange} required />
               </div>
               <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Schools/Deanery" type="text" name="schoolsDeanery" value={formData.schoolsDeanery} onChange={handleChange} />
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 ' placeholder="Year and Semester" type="text" name="yearAndSemester" value={formData.yearAndSemester} onChange={handleChange} required />
+              </div>
+              </div>
+            </div> 
+            <div className='mt-3 border border-black bg-gray-50 rounded'>
+            <h2 className='font-mono font-black text-2xl w-full font-bold text-blue-500  ml-3'>WORK DETAILS</h2>
+            <div className='grid grid-cols-3'>
+              <div className='m-2 p-2'>
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 ' placeholder="Sections handled" type="number" name="sectionsHandled" value={formData.sectionsHandled} onChange={handleChange} required />
+              </div>
+              <div className='m-2 p-2 flex  flex-wrap'>
+                <input className='w-80  border-black border rounded p-2 outline-blue-600 ' type="date" name="startDate" value={formData.startDate} onChange={handleChange} required />
               </div>
               <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Department" type="text" name="department" value={formData.department} onChange={handleChange} required />
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 ' placeholder="No: of Hours" type="number" name="hours" value={formData.hours} onChange={handleChange} required />
               </div>
               <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Subject Name" type="text" name="subjectName" value={formData.subjectName} onChange={handleChange} required />
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600 ' placeholder="Proposed Rate" type="number" name="proposedRate" value={formData.proposedRate} onChange={handleChange} required />
               </div>
               <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Year and Semester" type="text" name="yearAndSemester" value={formData.yearAndSemester} onChange={handleChange} required />
+                <input autoComplete='no' className='w-80  border-black border rounded p-2 outline-blue-600' placeholder="Total Amount" type="number" name="totalAmount" value={formData.totalAmount} onChange={handleChange} required />
               </div>
             </div>
-            <div className='bg-gray-100 rounded border-l-8  p-3 flex flex-col justify-items-start flex-wrap items-center'>
-              <h2 className='font-mono font-black text-2xl w-full border-sky-200 font-bold text-blue-500 col-span-2 text-center mb-3'>WORK DETAILS</h2>
-              <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Sections handled" type="number" name="sectionsHandled" value={formData.sectionsHandled} onChange={handleChange} required />
-              </div>
-              <div className='m-2 p-2 flex justify-center flex-wrap'>
-                <input className='w-80 border-l-8 border-blue-600 rounded p-3' type="date" name="startDate" value={formData.startDate} onChange={handleChange} required />
-              </div>
-              <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="No: of Hours" type="number" name="hours" value={formData.hours} onChange={handleChange} required />
-              </div>
-              <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Proposed Rate" type="number" name="proposedRate" value={formData.proposedRate} onChange={handleChange} required />
-              </div>
-              <div className='m-2 p-2'>
-                <input autoComplete='no' className='w-80 border-l-8 border-blue-600 rounded p-3 outline-blue-600' placeholder="Total Amount" type="number" name="totalAmount" value={formData.totalAmount} onChange={handleChange} required />
-              </div>
             </div>
           </div>
-          <div className='flex justify-center items-center'>
-            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
-            <button onClick={goBack} className="text-xs md:text-base ml-2 bg-blue-500 hover:bg-blue-700 text-white font-extrabold py-2 px-4 rounded">
-          Back
-        </button>
+          
+          <div className='flex justify-between gap-6'>
+          <button onClick={goBack} className="text-xs md:text-base ml-2 bg-blue-500 hover:bg-blue-700 text-white font-extrabold py-2 px-4 rounded">Back</button>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">Submit</button>
           </div>
         </form>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
